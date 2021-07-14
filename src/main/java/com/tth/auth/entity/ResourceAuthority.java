0f26e6076e3dec23396924b6d103e9a9edb68064
tuanth <tuanthse62716@gmail.com>
2021-07-14 23:28:04 +0700

    ResourceAuthority entity: custom join condition with @JoinFormula

--- src/main/java/com/tth/auth/entity/ResourceAuthority.java
+++ src/main/java/com/tth/auth/entity/ResourceAuthority.java
@@ -10,13 +10,13 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.tth.auth.dto.resourceAuthority.ResourceType;
 import com.tth.auth.entity.audit.ShortAuditEntity;
 
+import org.hibernate.annotations.JoinFormula;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import lombok.Getter;
@@ -43,23 +43,33 @@ public class ResourceAuthority extends ShortAuditEntity {
   private UUID targetId;
 
   @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "targetId",
-      referencedColumnName = "id",
-      insertable = false,
-      updatable = false)
-  private User user;
+  // @JoinColumn(name = "targetId", referencedColumnName = "id", 
+  //     insertable = false, updatable = false)
+  @JoinFormula(value = "(SELECT target_id WHERE target_type = 'USER')", 
+      referencedColumnName = "id")
+  private User targetUser;
 
   @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "targetId",
-      referencedColumnName = "id",
-      insertable = false,
-      updatable = false)
-  private Group group;
+  // @JoinColumn(name = "targetId", referencedColumnName = "id",
+  //     insertable = false, updatable = false)
+  @JoinFormula(value = "(SELECT target_id WHERE target_type = 'GROUP')", 
+      referencedColumnName = "id")
+  private Group targetGroup;
 
   @Enumerated(EnumType.STRING)
   private ResourceType resourceType;
 
   private String resourceId;
+  
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinFormula(value = "(SELECT resource_id::UUID WHERE resource_type = 'USER')", 
+      referencedColumnName = "id")
+  private User resourceUser;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinFormula(value = "(SELECT resource_id::UUID WHERE resource_type = 'GROUP')", 
+      referencedColumnName = "id")
+  private Group resourceGroup;
 
   private int permissions;
 
